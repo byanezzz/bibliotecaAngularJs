16327c221dd47255b193c9cf18c840ce6fcd2d97
Bianca <biancayanez89@gmail.com>
2018-05-29 22:29:58 -0400

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bibliotecaJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bibliotecaJsApp
+ */
+angular.module('bibliotecaJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
