354b71b6397bb49ec2b4a243c1a834684199dc9c
Bianca <biancayanez89@gmail.com>
2018-06-10 15:48:57 -0400

    change files names

---
+++ app/scripts/controllers/search_controller.js
@@ -0,0 +1,18 @@
+  (function(){
+      'use strict';
+  
+      angular
+          .module('bibliotecaJsApp')
+          .controller('SearchCtrl', SearchCtrl)
+  
+      SearchCtrl.$inject = ['$scope'];
+  
+      function SearchCtrl($scope) {
+          
+          var vm = this;
+          vm.name="Bianca";
+          activate();
+  
+          function activate() { }
+      }
+  })();
